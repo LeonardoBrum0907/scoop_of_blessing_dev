1c7d7628d1e70030467b11c0457f3252a7d6bc6a
Leandro Souza <leandro-araujo2003@hotmail.com>
2025-06-09 11:26:15 -0300

    adjustment responsive of section 2 3 and 4

--- src/components/Sections/Section2/index.jsx
+++ src/components/Sections/Section2/index.jsx
@@ -59,13 +59,13 @@ const Section2 = () => {
       // }, [])
 
       return (
-            <Section id="section-2" className="bg-background-color flex gap-8">
-                  <div className='flex flex-col gap-6'>
+            <Section id="section-2" className="bg-background-color flex flex-col md:flex-row gap-8">
+                  <div className='flex flex-col gap-6 items-center'>
                         <h1 className='text-orange text-[4rem] title'>A scoop of our story</h1>
                         <p className='text-text-color'>Think of us as your emotional support scoop — handcrafted with passion, scooped with pride, and taste-tested (maybe too much) for your happiness. Made with natural ingredients and plenty of love. Take a peek at some of our processes below.</p>
-                        <button className='border-2 border-orange px-4 py-2 rounded-full text-orange tracking-widest uppercase'>find out more</button>
+                        <button className='border-2 border-orange px-4 py-2 rounded-full text-orange  uppercase w-max tracking-[0.3rem]'>find out more</button>
                   </div>
-                  <div className='w-2/4 flex-shrink-0'>
+                  <div className='md:w-2/4 flex-shrink-0'>
                         <img src={sorveteimg} alt="imagem do sorvete" className="w-[100%]"/>
                   </div>
             </Section>
