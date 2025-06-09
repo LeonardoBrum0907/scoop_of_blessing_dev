8609eb23ed6515a76110bfa4ff657c87724e3db8
henriqueRufino <henriquerufino0504@gmail.com>
2025-06-08 22:08:31 -0300

    bugfix section 1 &upddate section 5

--- src/components/SectionLayout/index.jsx
+++ src/components/SectionLayout/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 const Section = ({className, children, id, props}) => {
   return (
-    <section id={id} className={`min-h-[100vh] px-4 py-10 sm:px-60 sm:py-[8rem] ${className}`} {...props}>
+    <section id={id} className={`min-h-[100vh] px-4 py-10 md:px-20 xl:px-60 md:py-[8rem] ${className}`} {...props}>
       {children}
     </section>
   )
