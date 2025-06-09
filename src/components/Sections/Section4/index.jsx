2834b703b5def44a4d88769c8f7db3f673cf1f26
Leandro Souza <leandro-araujo2003@hotmail.com>
2025-06-09 00:44:41 -0300

    starting section 4

--- src/components/Sections/Section4/index.jsx
+++ src/components/Sections/Section4/index.jsx
@@ -1,10 +1,13 @@
 import Section from "../../SectionLayout"
+import proposito from "/assets/imagenspipo/img_proposito.png";
 
 const Section4 = () => {
 
       return (
-            <Section id="section-4 " className="bg-pink">
-                  <h1>SESSÃO 4</h1>
+            <Section id="section-4 " className="bg-background-color flex flex-col items-center">
+                  <h1 className=" text-orange title text-[4rem] w-5/6 text-center">Crafted with care. Shared with love.</h1>
+                  <p className=" text-text-color w-5/6 text-[0.9rem] text-center">We craft outstanding ice cream to fund global missions, passionately dedicated to sharing the Gospel with over 3 billion unreached people. Because genuine ice cream blesses others.</p>
+                  <div className="w-full"><img src={proposito} alt="imagem proposito" /></div>
             </Section>
       )
 }
