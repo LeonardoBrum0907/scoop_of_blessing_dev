51837d71a1b627cbd495345fa0ebcf3c49476903
Leandro Souza <leandro-araujo2003@hotmail.com>
2025-06-09 11:44:41 -0300

    adjustment section 3

--- src/components/Sections/Section3/index.jsx
+++ src/components/Sections/Section3/index.jsx
@@ -39,7 +39,7 @@ const Section3 = () => {
 
       return (
             <Section id="section-3" className="bg-[url('/assets/imagenspipo/img_video_bg.png')] bg-cover bg-center flex items-center justify-center md:justify-end  gap-4">
-                  <div className='md:w-1/3 bg-background-color p-4 rounded-[2rem] md:bg-transparent'>
+                  <div className='md:w-2/5 bg-background-color p-4 rounded-[2rem] md:bg-transparent'>
                         <h1 className='mb-4 title text-pink text-[4rem]'>Team delicious</h1>
                         <p className='text-text-color mb-4'>Scoop of Blessing is a Brazilian-born (Matteo Sorveteria Criativa), award-winning craft ice cream company, now in the U.S., creating natural ice creams while empowering individuals with disabilities through strategic partnerships.</p>
                         <button className=' text-white bg-pink rounded-full py-2 px-6 uppercase tracking-[0.3rem]'>See the Magic Behind the Cone</button>
